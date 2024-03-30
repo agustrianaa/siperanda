7b49c19e43792f3d6b2697ca0a29e021ebd50782
agustrianaa <ritaagustrianamanik@gmail.com>
2024-03-30 11:14:21 +0700

    template

---
+++ resources/views/template.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIPERANDA</title>
+    <link rel="shortcut icon" type="image/png" href="../assets/images/logos/polindra.png" />
+    <link rel="stylesheet" href="../assets/css/styles.min.css" />
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+
+        <!-- SIDEBAR -->
+        @include('layouts.components.sidebar')
+
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+
+            <!-- HEADER/NAVBAR -->
+            @include('layouts.components.navbar')
+
+            <div class="container-fluid">
+                <!-- CONTENT -->
+                @yield('content')
+
+                <div class="py-6 px-6 text-center">
+                    <p class="mb-0 fs-4">Design and Developed by <a href="https://adminmart.com/" target="_blank" class="pe-1 text-primary text-decoration-underline">AdminMart.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/sidebarmenu.js"></script>
+    <script src="../assets/js/app.min.js"></script>
+    <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+    <script src="../assets/js/dashboard.js"></script>
+</body>
+
+</html>
